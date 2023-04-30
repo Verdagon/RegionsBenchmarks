6eed922bd6a3a14b63706904a83a25a93f5991ad
Evan Ovadia <verdagon@gmail.com>
2023-04-29 23:20:36 -0400

    Added CA

--- benchmark-build.sh
+++ benchmark-build.sh
@@ -1,13 +1,13 @@
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_unsafe_no_bounds --region_override unsafe-fast --include_bounds_checks false stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_unsafe_with_bounds --region_override unsafe-fast stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_naive_rc --region_override naive-rc --elide_checks_for_regions false stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_naive_atomic_rc --region_override naive-rc --elide_checks_for_regions false --use_atomic_rc true stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_rc_regions --region_override naive-rc stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_baseline --region_override safe --elide_checks_for_regions false stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_32 --region_override safe --gen_size 32 stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_64 --region_override safe --gen_size 64 stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_fastest --region_override safe-fastest stdlib=$1/stdlib/src || exit 1
-$1/valec build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_resilientv3 --region_override resilient-v3 stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_unsafe_no_bounds --region_override unsafe-fast --include_bounds_checks false stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_unsafe_with_bounds --region_override unsafe-fast stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_naive_rc --region_override naive-rc --elide_checks_for_regions false stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_naive_atomic_rc --region_override naive-rc --elide_checks_for_regions false --use_atomic_rc true stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_rc_regions --region_override naive-rc stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_baseline --region_override safe --elide_checks_for_regions false stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_32 --region_override safe --gen_size 32 stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_64 --region_override safe --gen_size 64 stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_fastest --region_override safe-fastest stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_resilientv3 --region_override resilient-v3 stdlib=$1/stdlib/src || exit 1
 
 echo ./build_unsafe_no_bounds/main:
 ./build_unsafe_no_bounds/main || exit 1
--- benchmark-run.sh
+++ benchmark-run.sh
@@ -5,9 +5,9 @@ hyperfine --warmup 30 --runs 500 "./build_unsafe_no_bounds/main" "./build_unsafe
 # Pay more attention to this one
 hyperfine --warmup 30 --runs 500 "./build_unsafe_no_bounds/main" "./build_unsafe_with_bounds/main" "./build_safe_baseline/main" "./build_safe_32/main" "./build_safe_64/main" "./build_safe_fastest/main" "./build_resilientv3/main"
 
-echo RC:
+# echo RC:
 
-# Do a run as a super-warmup
-hyperfine --warmup 30 --runs 500 "./build_unsafe_no_bounds/main" "./build_unsafe_with_bounds/main" "./build_rc_regions/main" "./build_naive_rc/main" "./build_naive_atomic_rc/main"
-# Pay more attention to this one
-hyperfine --warmup 30 --runs 500 "./build_unsafe_no_bounds/main" "./build_unsafe_with_bounds/main" "./build_rc_regions/main" "./build_naive_rc/main" "./build_naive_atomic_rc/main"
+# # Do a run as a super-warmup
+# hyperfine --warmup 30 --runs 500 "./build_unsafe_no_bounds/main" "./build_unsafe_with_bounds/main" "./build_rc_regions/main" "./build_naive_rc/main" "./build_naive_atomic_rc/main"
+# # Pay more attention to this one
+# hyperfine --warmup 30 --runs 500 "./build_unsafe_no_bounds/main" "./build_unsafe_with_bounds/main" "./build_rc_regions/main" "./build_naive_rc/main" "./build_naive_atomic_rc/main"
