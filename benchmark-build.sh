f3f446f6a5c93b0d7e2b3420ebecd64f1070d5b7
Evan Ovadia <>
2023-04-30 16:42:15 -0600

    Added cellular automata and better scripts

--- benchmark-build.sh
+++ benchmark-build.sh
@@ -1,13 +1,18 @@
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_unsafe_no_bounds --region_override unsafe-fast --include_bounds_checks false || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_unsafe_with_bounds --region_override unsafe-fast || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_naive_rc --region_override naive-rc --elide_checks_for_regions false || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_naive_atomic_rc --region_override naive-rc --elide_checks_for_regions false --use_atomic_rc true || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_rc_regions --region_override naive-rc || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_baseline --region_override safe --elide_checks_for_regions false || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_32 --region_override safe --gen_size 32 || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_64 --region_override safe --gen_size 64 || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_safe_fastest --region_override safe-fastest || exit 1
-$1 build rlzc=src --clang_override $2 --no_std true --opt_level O3 --asm true --llvm_ir true --output_dir build_resilientv3 --region_override resilient-v3 || exit 1
+# Usage: ./benchmark-build.sh path/to/vale/folder path/to/clang which-benchmark
+# Examples:
+#   ./benchmark-build.sh ~/Vale /usr/bin/clang tiny-roguelike
+#   ./benchmark-build.sh ~/Vale /usr/bin/clang cellular-automata
+
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_unsafe_no_bounds --region_override unsafe-fast --include_bounds_checks false stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_unsafe_with_bounds --region_override unsafe-fast stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_naive_rc --region_override naive-rc --elide_checks_for_regions false stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_naive_atomic_rc --region_override naive-rc --elide_checks_for_regions false --use_atomic_rc true stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_rc_regions --region_override naive-rc stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_baseline --region_override safe --elide_checks_for_regions false stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_32 --region_override safe --gen_size 32 stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_64 --region_override safe --gen_size 64 stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_safe_fastest --region_override safe-fastest stdlib=$1/stdlib/src || exit 1
+$1/valec build rlzc=$3 --clang_override $2 --no_std true --opt_level O2i --asm true --llvm_ir true --output_dir build_resilientv3 --region_override resilient-v3 stdlib=$1/stdlib/src || exit 1
 
 echo ./build_unsafe_no_bounds/main:
 ./build_unsafe_no_bounds/main || exit 1
